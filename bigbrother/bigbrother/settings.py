57388d37a5feb7fbea4ad7ab72a5599efa60c8bd
yenda <yenda1@gmail.com>
2014-09-29 06:14:13 +0200

    edit config

--- bigbrother/bigbrother/settings.py
+++ bigbrother/bigbrother/settings.py
@@ -115,6 +115,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 # Override settings with local settings.
 try:
