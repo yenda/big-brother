0cd152d6cd34a6e10d09fac0b198ad822c400723
yenda <yenda1@gmail.com>
2014-09-29 12:25:30 +0200

    fix parser

--- bigbrother/adeweb/backends/adeweb.py
+++ bigbrother/adeweb/backends/adeweb.py
@@ -14,14 +14,14 @@ class API(object):
         url = "https://adeweb.univ-lorraine.fr/jsp/webapi?function=connect&login=ade_projet_etu&password=;projet_2014"
         response = urlopen(url)
 
-        xmldoc = parse(response)
+        xmldoc = parse(url)
         self.sessionId = xmldoc.getElementsByTagName('session')[0].attributes['id'].value
 
     def disconnect(self):
         url = "https://adeweb.univ-lorraine.fr/jsp/webapi?sessionId="+self.sessionId+"&function=disconnect"
         response = urlopen(url)
 
-        xmldoc = parse(response)
+        xmldoc = parse(url)
         if xmldoc.getElementsByTagName('disconnected')[0].attributes['sessionId'].value == self.sessionId:
             self.sessionId = None
             return self.sessionId
@@ -30,9 +30,6 @@ class API(object):
     def set_project(self):
         url = "https://adeweb.univ-lorraine.fr/jsp/webapi?sessionId="+self.sessionId+"&function=setProject&projectId=9"
         response = urlopen(url)
-
-        project = response.read()
-        return project
 
     def get_resources(self):
         url = "https://adeweb.univ-lorraine.fr/jsp/webapi?sessionId="+self.sessionId+"&function=getResources&detail=0"
