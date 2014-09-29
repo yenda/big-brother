48e6b17cf8aaf53aac18a83ed8eea485ff7026dc
yenda <yenda1@gmail.com>
2014-09-29 13:13:42 +0200

    fix url

--- bigbrother/cal/urls.py
+++ bigbrother/cal/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import EventView, CreateEventView, CalendarView
 
 urlpatterns = patterns('',
-                       url(r'^/$', CalendarView.as_view(), name='calendar'),
+                       url(r'^$', CalendarView.as_view(), name='calendar'),
                        url(r'^event/create/$', CreateEventView.as_view(), name='create-event'),
                        url(r'^event/(?P<event>[- \w]+)/', EventView.as_view(), name='event'),
                        )
